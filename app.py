e7c352892abfe3fcd404d996c3576592ffb376af
Michael baburyan <baburyanmichael@gmail.com>
2023-10-11 19:54:55 -0400

    commit changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
@@ -13,3 +13,5 @@ def profile():
 
 if __name__ == '__main__':
    app.run(debug = True)
+   
+# backend test
