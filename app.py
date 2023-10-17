d032ce269684ecc3f746fa8c60916ebfd6033bdc
Michael baburyan <baburyanmichael@gmail.com>
2023-10-17 00:50:52 -0400

    post commit 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request, session, flash
-from flask_sqlalchemy import SQLAlchemy
+from table_info import profiles
+from db import db
 
 from datetime import timedelta, datetime
 
@@ -112,40 +113,19 @@ from datetime import timedelta, datetime
 app = Flask(__name__)
 
 app.secret_key = 'key'
-app.permanent_session_lifetime = timedelta(seconds=15)
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class profiles(db.Model):
-   _id = db.Column('id', db.Integer, primary_key = True)
-   username = db.Column(db.String(64), unique=True)
-   password = db.Column(db.String(64))
-   display_name = db.Column(db.String(128))
-   birthday = db.Column(db.String(64))
-   user_type = db.Column(db.String(64))
-   account_info = db.Column(db.Integer)
-   account_value = db.Column(db.Integer)
+db.init_app(app)
    
-   def __init__(self, username, password, display_name, birthday, user_type, account_info, account_value):
-      self.username = username
-      self.password = password
-      self.display_name = display_name
-      self.birthday = birthday
-      self.user_type = user_type
-      self.account_info = account_info
-      self.account_value = account_value
-   
-
 class profile_update():
    def delete_user(temp_username):
       profiles.query.filter_by(username = temp_username).delete()
       db.session.commit()
       
       
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
    if request.method == 'GET':
@@ -161,10 +141,19 @@ def login():
       
       found_user = profiles.query.filter(profiles.username == current_user, profiles.password == current_password).first()
       if found_user:
+         session['type'] = found_user.user_type
+         
          return redirect(url_for('profile', username=session['username']))
       else:
          session.clear()
          return redirect(url_for('create_profile'))
+      
+@app.route('/logout')
+def logout():
+   if 'username' in session:
+      session.clear()
+   return redirect(url_for('login'))      
+
 
 @app.route('/create_profile', methods=['GET', 'POST'])
 def create_profile():
---
+++ table_info.py
@@ -0,0 +1,33 @@
+from db import db
+from datetime import datetime
+
+class profiles(db.Model):
+   _id = db.Column('id', db.Integer, primary_key = True)
+   username = db.Column(db.String(64), unique=True, nullable=False)
+   password = db.Column(db.String(64))
+   display_name = db.Column(db.String(128))
+   birthday = db.Column(db.String(64))
+   user_type = db.Column(db.String(64))
+   account_info = db.Column(db.Integer)
+   account_value = db.Column(db.Integer)
+   post = db.relationship('posts', backref='profiles', lazy=True)
+   
+   def __init__(self, username, password, display_name, birthday, user_type, account_info, account_value):
+      self.username = username
+      self.password = password
+      self.display_name = display_name
+      self.birthday = birthday
+      self.user_type = user_type
+      self.account_info = account_info
+      self.account_value = account_value
+
+class posts(db.Model):
+   _id = db.Column('id', db.Integer, primary_key=True)
+   content = db.Column(db.Text, nullable=False)
+   time_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+   
+   def __init__(self, content):
+      self.content = content
+      
+   
+
